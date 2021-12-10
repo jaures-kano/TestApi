d880749944fbb8a3389fd07e6e3de602a4cc8c11
Jaures Kano <ruddyjaures@gmail.com>
2021-12-10 09:11:58 +0100

    [feat]: configuration de la connexion via l'api

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,36 @@
 
 namespace App\Controller;
 
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/api/login", name="api_route_login")
+     */
+    public function indexApiLogin(JWTTokenManagerInterface $jwt)
+    {
+        $user = $this->getUser();
+        if ($user !== null) {
+            $token = $jwt->create($user);
+            return $this->json([
+                'message' => 'success!',
+                'user' => $user,
+                'token' => sprintf('Bearer %s', $token),
+            ]);
+        }
+
+        return new JsonResponse([
+            'message' => 'Invalide credential'
+        ], 401);
+    }
+
     /**
      * @Route("/", name="app_login")
      */
@@ -31,6 +54,6 @@ class SecurityController extends AbstractController
      */
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
---
+++ src/Security/ApiAuthentificator.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+
+class ApiAuthentificator extends AbstractAuthenticator
+{
+
+    /**
+     * @param Request $request
+     * @return bool|null
+     */
+    public function supports(Request $request): ?bool
+    {
+        return true;
+    }
+
+    /**
+     * @param Request $request
+     * @return Passport
+     */
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password', ''))
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $firewallName
+     * @return Response|null
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return Response|null
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new JsonResponse([
+            'message' => $exception->getMessage()
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+}
