c92c9b5c45578af7b0ecb7f8164af9ec106fd05b
Mani Crescence <crescencegracemani@gmail.com>
2021-12-22 18:23:08 +0100

    [feat] : integration d'un dashboard

--- src/Http/App/Controller/Admin/AdminController.php
+++ src/Http/App/Controller/Admin/AdminController.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\App\Controller\Admin;
 
-class AdminController
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AdminController extends AbstractController
 {
+    /**
+     * @Route("/admin", name="admin_index")
+     */
+    public function index() : Response
+    {
+        return $this->render("admin/base_admin/base_admin_index.html.twig");
+    }
 
 }
