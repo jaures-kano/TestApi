c087a1500ee2cdba767d93766bbbb78549804597
Mani Crescence <crescencegracemani@gmail.com>
2021-12-23 17:58:59 +0100

    [feat]:integration du dashboard

---
+++ assets/admin.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+
+ */
+import './vendors/js/vendor.bundle.base.js';
+import './vendors/chart.js/Chart.min.js';
+import './vendors/jquery-circle-progress/js/circle-progress.min.js';
+//import './js/jquery.cookie.js';
+import './js/off-canvas.js';
+//import './js/hoverable-collapse.js';
+import './js/misc.js';
+import './js/dashboard.js';
+
+
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import '../../styles/app.scss';
+
