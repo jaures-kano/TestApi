5ca90bf62a26fb0675d8e67f0823da024f33a5e5
Mani Crescence <crescencegracemani@gmail.com>
2021-12-23 17:59:13 +0100

    [feat]:integration du dashboard

--- assets/admin.js
+++ assets/admin.js
@@ -5,17 +5,12 @@
  * (and its CSS file) in your base layout (base.html.twig).
 
  */
-import './vendors/js/vendor.bundle.base.js';
-import './vendors/chart.js/Chart.min.js';
-import './vendors/jquery-circle-progress/js/circle-progress.min.js';
-//import './js/jquery.cookie.js';
-import './js/off-canvas.js';
-//import './js/hoverable-collapse.js';
-import './js/misc.js';
-import './js/dashboard.js';
+import './admin/vendors/js/vendor.bundle.base.js';
+import './admin/js/misc.js';
+import './admin/js/dashboard.js';
 
 
 
 // any CSS you import will output into a single css file (app.scss in this case)
-import '../../styles/app.scss';
+import './styles/app.scss';
 
--- src/Http/App/Controller/Admin/AdminController.php
+++ src/Http/App/Controller/Admin/AdminController.php
@@ -7,6 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @package App\Http\App\Controller\Admin
+ * @author Catherine Mani<crescencegracemani@gmail.com>
+ */
+
 
 class AdminController extends AbstractController
 {
