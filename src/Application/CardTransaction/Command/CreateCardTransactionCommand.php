d660ba5764afbbc6016fdc4e929ef40d6ec1583b
Maxime <elessamaxime@icloud.com>
2021-12-29 10:55:55 +0100

    [feat] : CreateCardTransaction command

---
+++ src/Application/CardTransaction/Command/CreateCardTransactionCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Application\CardTransaction\Command;
+
+
+use App\Application\CardTransaction\Dto\CardTransactionDto;
+use App\Domain\CardTransaction\Entity\CardTransaction;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * @author Elessa Maxime <elessamaxime@icloud.com>
+ * @package App\Application\CardTransaction\Command
+ */
+class CreateCardTransactionCommand
+{
+    public function create(CardTransactionDto $cardTransactionDto, EntityManagerInterface $manager)
+    {
+        $cardTransaction = new CardTransaction();
+
+        $cardTransaction->setUser($cardTransactionDto->user)
+            ->setAmount($cardTransactionDto->amount)
+            ->setType($cardTransactionDto->type)
+            ->setCard($cardTransactionDto->card)
+            ->setFees($cardTransactionDto->fees)
+            ->setRecipient($cardTransactionDto->recipient)
+            ->setReferenceNumber($cardTransactionDto->referenceNumber)
+            ->setTransmitter($cardTransactionDto->transmitter)
+            ->setCreatedAt($cardTransactionDto->createdAt)
+            ->setUpdatedAt($cardTransactionDto->updatedAt)
+        ;
+    }
+}
