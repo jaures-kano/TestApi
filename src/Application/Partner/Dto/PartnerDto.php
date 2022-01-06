b93ad661516d1b75039edc69f65d4ba5781614e3
Maxime <elessamaxime@icloud.com>
2022-01-06 15:46:57 +0100

    [feat] : Partner Dto and Command

---
+++ src/Application/Partner/Dto/PartnerDto.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Application\Partner\Dto;
+
+
+use App\Domain\Auth\Entity\User;
+use App\Domain\Partner\Entity\Partner;
+use DateTimeInterface;
+
+/**
+ * @author Elessa Maxime <elessamaxime@icloud.com>
+ * @package App\Application\Partner\Dto
+ */
+class PartnerDto
+{
+    public ?DateTimeInterface $updatedAt;
+    public ?DateTimeInterface $createdAt;
+    public ?string $code;
+    public ?User $user;
+
+    public function __construct(Partner $partner = null)
+    {
+        $this->user = $partner == null ? null : $partner->getUser();
+        $this->code = $partner == null ? null : $partner->getCode();
+        $this->createdAt = $partner == null ? null : $partner->getCreatedAt();
+        $this->updatedAt = $partner == null ? null : $partner->getUpdatedAt();
+    }
+}
